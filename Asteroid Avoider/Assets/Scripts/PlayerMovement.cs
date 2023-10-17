df63b04e167bec994fc7d159fe23cc7caa00f92b
fatmaCevik <cevikfatma93@gmail.com>
2023-10-17 20:35:10 +0300

    Spaceship Movement

--- Asteroid Avoider/Assets/Scripts/PlayerMovement.cs
+++ Asteroid Avoider/Assets/Scripts/PlayerMovement.cs
@@ -3,22 +3,40 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float forceMagnitude;
+    [SerializeField] private float maxVelocity;
+
     private Camera mainCamera;
+    private Vector3 movementDirection;
    
-    void Start()
+    private void Start()
     {
         mainCamera = Camera.main;
     }
 
-    void Update()
+    private void Update()
     {
         if(Touchscreen.current.primaryTouch.press.isPressed)
         {
             Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
-            Debug.Log(touchPosition);
-
             Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
-            Debug.Log(worldPosition);
+
+            movementDirection = transform.position - worldPosition;
+            movementDirection.z = 0f;
+            movementDirection.Normalize();
         }
+        else
+        {
+            movementDirection = Vector3.zero;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (movementDirection == Vector3.zero)
+            return;
+        rb.AddForce(movementDirection *  forceMagnitude * Time.deltaTime, ForceMode.Force);
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxVelocity);
     }
 }
