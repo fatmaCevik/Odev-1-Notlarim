0e957e6ba6dab5158111ffb149f2d4880b882c16
fatmaCevik <cevikfatma93@gmail.com>
2023-10-18 16:55:09 +0300

    Player Staying on Screen
    Keeping the player on screen for each device. But the player's body is not positioned in the direction he is going.

--- Asteroid Avoider/Assets/Scripts/PlayerMovement.cs
+++ Asteroid Avoider/Assets/Scripts/PlayerMovement.cs
@@ -17,7 +17,22 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        if(Touchscreen.current.primaryTouch.press.isPressed)
+        ProcessInput();
+        KeepPlayerOnScreen();
+    }
+
+    private void FixedUpdate()
+    {
+        if (movementDirection == Vector3.zero)
+            return;
+        rb.AddForce(movementDirection *  forceMagnitude * Time.deltaTime, ForceMode.Force);
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxVelocity);
+    }
+
+    private void ProcessInput()
+    {
+
+        if (Touchscreen.current.primaryTouch.press.isPressed)
         {
             Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
             Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
@@ -32,11 +47,30 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private void FixedUpdate()
+    private void KeepPlayerOnScreen()
     {
-        if (movementDirection == Vector3.zero)
-            return;
-        rb.AddForce(movementDirection *  forceMagnitude * Time.deltaTime, ForceMode.Force);
-        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxVelocity);
+        Vector3 newPosition = transform.position;
+        Vector3 viewportPosition = mainCamera.WorldToViewportPoint(transform.position);
+
+        if(viewportPosition.x > 1)
+        {
+            newPosition.x = -newPosition.x + 0.1f;
+        }
+        else if(viewportPosition.x < 0)
+        {
+            newPosition.x = -newPosition.x - 0.1f;
+        }
+
+        if (viewportPosition.y > 1)
+        {
+            newPosition.y = -newPosition.y + 0.1f;
+        }
+        else if(viewportPosition.y < 0)
+        { 
+            newPosition.y = -newPosition.y - 0.1f;
+        }
+
+        transform.position = newPosition;
     }
 }
+
