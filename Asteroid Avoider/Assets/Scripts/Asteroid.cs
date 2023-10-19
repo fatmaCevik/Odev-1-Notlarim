2ebdfcc2ed1a57b9a796884748ebc53f61930726
fatmaCevik <cevikfatma93@gmail.com>
2023-10-19 16:11:07 +0300

    Gardual Health Reducation Trial
    Typed commands left as comment lines in script files

--- Asteroid Avoider/Assets/Scripts/Asteroid.cs
+++ Asteroid Avoider/Assets/Scripts/Asteroid.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Asteroid : MonoBehaviour
@@ -13,5 +11,6 @@ public class Asteroid : MonoBehaviour
             return;
 
         playerHealth.Crash();
+        //playerHealth.RemainingLife();
     }
 }
--- Asteroid Avoider/Assets/Scripts/PlayerHealth.cs
+++ Asteroid Avoider/Assets/Scripts/PlayerHealth.cs
@@ -2,8 +2,22 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
+    //[SerializeField] private int health;
+    
+    
     public void Crash()
     {
         gameObject.SetActive(false);
     }
+    
+    /*
+    public void RemainingLife()
+    {
+        if (health > 0)
+            health -= health;
+        else
+            gameObject.SetActive(false);
+    }
+    */
 }
+
