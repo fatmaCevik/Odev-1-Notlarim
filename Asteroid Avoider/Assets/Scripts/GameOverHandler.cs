85df4146ef446cc794808abe04e91728d255d941
fatmaCevik <cevikfatma93@gmail.com>
2023-10-24 23:09:29 +0300

    Score System

--- Asteroid Avoider/Assets/Scripts/GameOverHandler.cs
+++ Asteroid Avoider/Assets/Scripts/GameOverHandler.cs
@@ -1,14 +1,20 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameOverHandler : MonoBehaviour
 {
+    [SerializeField] private TMP_Text gameOverText;
+    [SerializeField] private ScoreSystem scoreSystem;
     [SerializeField] private GameObject gameOverDisplay;
     [SerializeField] private AsteroidSpawner asteroidSpawner;
 
     public void EndGame()
     {
         asteroidSpawner.enabled = false;
+
+        int finalScore = scoreSystem.EndTimer();
+        gameOverText.text = $"Your score: {finalScore}";
 
         gameOverDisplay.gameObject.SetActive(true);
     }
---
+++ Asteroid Avoider/Assets/Scripts/ScoreSystem.cs
@@ -0,0 +1,27 @@
+using TMPro;
+using UnityEngine;
+
+public class ScoreSystem : MonoBehaviour
+{
+    [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private float scoreMultiplier;
+
+    private bool shouldCount = true;
+    private float score;
+    public const string CurrentScoreKey = "CurrentScore";
+
+    private void Update()
+    {
+        if (!shouldCount)
+            return;
+        score += Time.deltaTime * scoreMultiplier;
+        scoreText.text = Mathf.FloorToInt(score).ToString();
+    }
+
+    public int EndTimer()
+    {
+        shouldCount = false;
+        scoreText.text = string.Empty;
+        return Mathf.FloorToInt(score);
+    }
+}
